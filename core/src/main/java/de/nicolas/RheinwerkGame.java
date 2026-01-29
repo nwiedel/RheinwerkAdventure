f1e2bb1707f84e554697559991dc4c2386ec7951
nwiedel <164350832+nwiedel@users.noreply.github.com>
2026-01-29 08:13:43 +0100

    2. Utilis Klassen hinzugefügt

--- core/src/main/java/de/nicolas/RheinwerkGame.java
+++ core/src/main/java/de/nicolas/RheinwerkGame.java
@@ -1,32 +1,14 @@
 package de.nicolas;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import de.nicolas.screens.LevelScreen;
+import de.nicolas.utils.game.BaseGame;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class RheinwerkGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class RheinwerkGame extends BaseGame {
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
+        super.create();
+        setActiveScreen(new LevelScreen());
     }
 }
---
+++ core/src/main/java/de/nicolas/screens/LevelScreen.java
@@ -0,0 +1,15 @@
+package de.nicolas.screens;
+
+import de.nicolas.utils.screens.BaseScreen;
+
+public class LevelScreen extends BaseScreen {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+}
